380afc13ac84509c3a55140a89406eb40fede85a
Zabi <zbvmihai@gmail.com>
2023-03-25 17:59:24 +0000

    Added Sign Up And Sign In Functionality
    -Added functionality to sign up with user and password
    -Added functionality to chef is password match with confirm password and check if the password is 8 characters long, have at least 1 number and 1 capital letter.
    -Added the functionality to chef if user filled all the sign up fields.
    -Added functionality to add the user in Realtime database.
    -Added functionality to sign in with user and password.

--- app/src/main/java/com/finecut/barbershop/models/Users.kt
+++ app/src/main/java/com/finecut/barbershop/models/Users.kt
@@ -1,3 +1,42 @@
 package com.finecut.barbershop.models
 
-data class Users()
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Users(
+    val id: String = "",
+    val firstName: String = "",
+    val surname: String = "",
+    val email: String = "",
+    val phoneNumber: Long = 0
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readLong()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(firstName)
+        parcel.writeString(surname)
+        parcel.writeString(email)
+        parcel.writeLong(phoneNumber)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Users> {
+        override fun createFromParcel(parcel: Parcel): Users {
+            return Users(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Users?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
