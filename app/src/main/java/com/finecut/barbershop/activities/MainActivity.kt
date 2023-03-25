7b0a8a4dd4376e3d6c7e901380b6c00614ae1abc
Zabi <zbvmihai@gmail.com>
2023-03-25 19:21:01 +0000

    -Add toolbar and on back pressed functionality
    -Added toolbar
    -Added toolbar title
    -Added on back press twice functionality to exit the app
    -Added the functionality on login activity to check if the user is already logged in.

--- app/src/main/java/com/finecut/barbershop/activities/MainActivity.kt
+++ app/src/main/java/com/finecut/barbershop/activities/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.finecut.barbershop.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 import com.finecut.barbershop.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
+    private var backPressedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +18,48 @@ class MainActivity : AppCompatActivity() {
         val view = mainBinding.root
         setContentView(view)
 
+        setupActionBar()
+        setupOnBackPressedCallback()
+    }
 
+    private fun setupActionBar() {
+        setSupportActionBar(mainBinding.tbMain)
+
+        val actionBar = supportActionBar
+
+        actionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    private fun setupOnBackPressedCallback() {
+        val onBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (backPressedTime + 2000 > System.currentTimeMillis()) {
+                    isEnabled = false
+                    finish()
+                } else {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Press back again to close the app",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                backPressedTime = System.currentTimeMillis()
+            }
+        }
+
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
     }
 }
+
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return when (item.itemId) {
+//            android.R.id.home -> {
+//
+//                Toast.makeText(this@MainActivity,"Back Clicked",Toast.LENGTH_SHORT).show()
+//                true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
+//}
