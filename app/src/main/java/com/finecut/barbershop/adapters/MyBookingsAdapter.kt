e4a8cfe76106be783cbc7ad52f07f6055ee2a22e
Zabi <zbvmihai@gmail.com>
2023-04-05 16:38:27 +0100

    Added functionality to cancel a booking.
    -Added the functionality in My Bookings activity to cancel the booking.

--- app/src/main/java/com/finecut/barbershop/adapters/MyBookingsAdapter.kt
+++ app/src/main/java/com/finecut/barbershop/adapters/MyBookingsAdapter.kt
@@ -1,12 +1,14 @@
 package com.finecut.barbershop.adapters
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.finecut.barbershop.R
@@ -16,11 +18,15 @@ import com.finecut.barbershop.models.Barbers
 import com.finecut.barbershop.models.Bookings
 import com.finecut.barbershop.utils.FirebaseData
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.squareup.picasso.Picasso
 
 class MyBookingsAdapter(private var context: Context,
     private var bookingsList: ArrayList<Bookings>
     ):RecyclerView.Adapter<MyBookingsAdapter.BookingsViewHolder>() {
+
+    private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
 
     inner class BookingsViewHolder(val adapterBookings: BookingsCardBinding)
         : RecyclerView.ViewHolder(adapterBookings.root)
@@ -31,6 +37,16 @@ class MyBookingsAdapter(private var context: Context,
     }
 
     override fun onBindViewHolder(holder: BookingsViewHolder, position: Int) {
+
+        val bookingDate = "${bookingsList[holder.adapterPosition].date}-${bookingsList[holder.adapterPosition].timeslot.replace(":", "-")}"
+
+        val userBookingRef: DatabaseReference = firebaseDatabase.getReference("Users")
+            .child(bookingsList[holder.adapterPosition].userId)
+            .child("Bookings").child(bookingDate)
+
+        val barberBookingRef: DatabaseReference = firebaseDatabase.getReference("Barbers")
+            .child(bookingsList[holder.adapterPosition].barberId)
+            .child("Bookings").child(bookingDate)
 
         FirebaseData.DBHelper.getBarberFromDatabase(bookingsList[holder.adapterPosition].barberId, object: FirebaseData.DBHelper.BarberCallback{
             @SuppressLint("SetTextI18n")
@@ -57,12 +73,47 @@ class MyBookingsAdapter(private var context: Context,
                         holder.adapterBookings.tvBookingStatus.setTextColor(ContextCompat.getColor(context, R.color.green))
 
                     }
-                    2 -> {holder.adapterBookings.tvBookingStatus.text = "Completed"
+                    2 -> {
+                        holder.adapterBookings.btnCancelBooking.visibility = View.GONE
+                        holder.adapterBookings.tvBookingStatus.text = "Completed"
                         holder.adapterBookings.tvBookingStatus.setTextColor(ContextCompat.getColor(context, R.color.salmon_pink))
                     }
                 }
 
-                Picasso.get().load(barber.image.ifEmpty { context.getString(R.string.userImagePlaceHolder)}).into(holder.adapterBookings.ivBarberImage)
+                Picasso.get().load(barber.image.ifEmpty { context.getString(R.string.userImagePlaceHolder)})
+                    .into(holder.adapterBookings.ivBarberImage)
+
+                holder.adapterBookings.btnCancelBooking.setOnClickListener {
+
+                    val alertDialogBuilder = AlertDialog.Builder(context)
+                    alertDialogBuilder.setTitle("Cancel Booking")
+                    alertDialogBuilder.setMessage("Are you sure you want to cancel this booking?")
+
+                    alertDialogBuilder.setPositiveButton("Yes") { _, _ ->
+                        userBookingRef.removeValue().addOnSuccessListener {
+                            Toast.makeText(
+                                context.applicationContext,
+                                "Booking canceled!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            bookingsList.removeAt(holder.adapterPosition)
+                            notifyItemRemoved(holder.adapterPosition)
+
+                        }.addOnFailureListener {
+                            it.localizedMessage?.let { it2 -> Log.e("Database Error:", it2) }
+                        }
+
+                        barberBookingRef.removeValue()
+                    }
+                    alertDialogBuilder.setNegativeButton("No") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+
+                    val alertDialog = alertDialogBuilder.create()
+                    alertDialog.show()
+
+                }
 
                 if(bookingsList[holder.adapterPosition].bookStatus == 2){
 
